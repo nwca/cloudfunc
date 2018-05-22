39b4ee8c41b2fc89df721ec31b786696f91fda44
Denys Smirnov <denys@sourced.tech>
2018-05-22 21:24:00 +0300

    trim url prefix for HTTP handlers

--- gcp/nodego/http.go
+++ gcp/nodego/http.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func HandleHTTP(fnc http.HandlerFunc) {
-	http.HandleFunc("/", fnc)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.TrimPrefix(r.URL.Path, executePrefix)
+		fnc(w, r)
+	})
 }
